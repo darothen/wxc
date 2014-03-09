ab2c36e84a82372c4063f316df8673a57a063962
Daniel Rothenberg <darothen@mit.edu>
2014-03-09 17:56:06 -0400

    Added data module as temporary holder for station names

---
+++ mosobs/util/data.py
@@ -0,0 +1,19 @@
+## Pre-defined flags
+MISSING = 9999
+
+STATIONS = { 'ksdf': 'LOUISVILLE INTERNATIONAL AIRPORT KY US',
+             'ksyr': 'SYRACUSE HANCOCK INTERNATIONAL AIRPORT NY US', }
+
+# 1 day timedelta
+ONE_DAY = datetime.timedelta(days=1)
+
+# NWSO location closest to each station?
+STATION_CODES = { 'khou': 'USC00414333',
+                  'kcys': 'USC00481676',
+                  'nrmn': 'USC00346382', 
+                  'kgrr': 'USC00203337',
+                  'nrmn': 'USC00346382',
+                  'kokc': 'USW00013967',
+                  'kcon': 'USW00014745',
+                  'krdd': 'USW00024257',
+                  }
